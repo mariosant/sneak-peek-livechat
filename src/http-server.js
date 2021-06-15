76d7c89e8f8486b82b198e96882c64c57c8db2de
Marios <mariosant@sent.com>
2021-06-15 23:34:03 +0300

    Add checks file

--- src/http-server.js
+++ src/http-server.js
@@ -2,15 +2,21 @@ const Koa = require("koa");
 const Router = require("@koa/router");
 const webhooks = require("./http/webhooks-livechat");
 const registration = require("./http/registration");
+const health = require("./http/health");
 const worker = require("./worker/index");
+const envMiddlware = require("./lib/env-middleware");
+
 
 const port = process.env.PORT ?? 3000;
 
 const app = new Koa();
+app.use(envMiddlware)
+
 const router = new Router();
 
 router.get("/auth/callback", registration);
 router.post("/webhooks", webhooks);
+router.get("/health", health);
 app.use(router.routes());
 
 worker.on("failed", console.log);
